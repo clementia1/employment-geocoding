04ec26681d353696582fd9607bc186050cdbc555
clementia1 <grininma0@gmail.com>
2019-07-26 17:01:23 +0300

    *

--- src/server/geocode script.js
+++ src/server/geocode script.js
@@ -12,7 +12,7 @@ let options = {
  
 let geocoder = NodeGeocoder(options); 
 
-let workbook = XLSX.readFile('filterResult_298475076.xls');
+let workbook = XLSX.readFile('data.xls');
 let sheet = workbook.Sheets[workbook.SheetNames[0]];
 let json = XLSX.utils.sheet_to_json(sheet);
 
@@ -21,25 +21,32 @@ function formatAddress(str) {
 		let newString = str.replace(/вул\./i, ' вулиця ');
 		newString = newString.replace(/(пр-т|пр)(\s|\.)/i, ' проспект ');
 		newString = newString.replace(/пров(\s|\.)/i, ' провулок ');
+		newString = newString.replace(/Харківська область, Харків, НОВОБАВАРСЬКИЙ/i, 'Харків');
+		newString = newString.replace(/Харківська область, Харків, ІНДУСТРІАЛЬНИЙ/i, 'Харків');
 		return newString;
 	}
 }
 
-json.forEach((item) => {
+// console.log(formatAddress('Харківська область, Харків, ІНДУСТРІАЛЬНИЙ, пр-т Московський, 275')); 
+
+json.map((item) => {
 	let str = item['Фактична адреса ПОУ / Оперативні вакансії'];
-	
+	let elem = {};
 	setTimeout(function() {
 		geocoder.geocode(formatAddress(str))
   			.then(function(res) {
-    			console.log(res);
+    			elem = res[0];
+    			console.log(elem);
+    			return elem
   			})
   			.catch(function(err) {
     			console.log(err);
   			});
 	}, 2500);
-
+	
+	return elem
 });
- /* formatAddress(str)
+ /* 
 geocoder.geocode('29 champs elysée paris')
   .then(function(res) {
     console.log(res);
@@ -48,3 +55,4 @@ geocoder.geocode('29 champs elysée paris')
     console.log(err);
   });
 */
+console.log(json);
