8017e11ba4812ad44e2a5e28710bcecdb8d24181
clementia1 <grininma0@gmail.com>
2019-08-02 10:51:28 +0300

    *

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Sidebar from "react-sidebar";
+import SidebarContent from "./SidebarContent.js";
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -6,7 +8,8 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-                data: []
+                data: [],
+                sidebarOpen: false
             }
     }
     
@@ -22,12 +25,25 @@ class App extends React.Component {
   			})
     }
 
+  	onSetSidebarOpen = (open) => {
+    	this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  	}
+
     render() {
-    	const position = [50.0020973, 36.2508555889877]
+    	const position = [50.0020973, 36.2508555889877];
+    	
         return (
         	<div className='container-fluid'>
         		<div className='map'>
         			<div className="row justify-content-center">
+        			<div >
+        				      <Sidebar
+        				      		sidebar={<SidebarContent/>}
+        							open={this.state.sidebarOpen}
+        							onSetOpen={this.onSetSidebarOpen}
+        							styles={{ sidebar: { background: "white", zIndex: 9999, width: '400px' } }} >
+      						  </Sidebar>
+      				</div>
         			<div className="col-12">
     					  					<Map maxZoom={19} center={position} zoom={13}>    					  						
         										<TileLayer
@@ -37,7 +53,7 @@ class App extends React.Component {
         										{
         											this.state.data.map(item => {        											
         												let position = [item.latitude, item.longitude]
-        												return <Marker position={position}><Popup>{item['Посада (назва) / Характеристика вакансії']}<br/>{item['Роботодавець (назва) / Оперативні вакансії']}<br/>{item['Заробітна плата / Оперативні вакансії']+' грн'}<br/>{item["Завдання та обов'язки / Характеристика вакансії"]}<br/><br/><button type="button" class="btn btn-outline-info btn-block">Мені цікаво</button></Popup></Marker>
+        												return <Marker position={position}><Popup>{item['Посада (назва) / Характеристика вакансії']}<br/>{item['Роботодавець (назва) / Оперативні вакансії']}<br/>{item['Заробітна плата / Оперативні вакансії']+' грн'}<br/>{item["Завдання та обов'язки / Характеристика вакансії"]}<br/><br/><button type="button" className="btn btn-outline-info btn-block" onClick={() => this.onSetSidebarOpen(true)}>Мені цікаво</button></Popup></Marker>
         											})
         										}
   											</Map>
---
+++ src/components/SidebarContent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class SidebarContent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            }
+    }
+    
+    
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+        	<div>
+        		<form className="custom-form">
+        			<div className="form-row custom-padding">Якщо вас зацікавила вакансія, залиште ваші контактні дані для зворотного зв'язку
+        			</div>
+  					<div className="form-row">
+    					<div className="form-group col-md-6">
+      						<input type="email" className="form-control" id="inputEmail4" placeholder="Ім'я"/>
+    					</div>
+    					<div className="form-group col-md-6">
+      						<input type="password" className="form-control" id="inputPassword4" placeholder="Фамілія"/>
+    					</div>
+  					</div>
+  					<div className="form-group">
+    					<input type="text" className="form-control" id="inputAddress" placeholder="Контактний телефон"/>
+  					</div>
+					<div class="custom-file">
+  					<input type="file" class="custom-file-input" id="customFile"/>
+  					<label class="custom-file-label" for="customFile">Додати резюме</label>
+					</div>
+  					<button type="submit" className="btn btn-primary">Відіслати</button>
+				</form>
+  			</div>
+        );
+    }
+}
+
+export default SidebarContent;
